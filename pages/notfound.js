17a5e2af0e751dacb8152e3a4549570424c30f0e
Generoso Cantu <gcantuw@gmail.com>
2020-04-07 13:59:45 -0500

    Test

--- pages/notfound.js
+++ pages/notfound.js
@@ -6,7 +6,7 @@ export default function NotFound() {
     <Layout>
       <p>Page not Found</p>
       <Link href="/section/[section]/[date]/[id]" as="/section/world-middleeast/2020-01-11/iraq-iran-us-troops-4c50e545-539e-4893-b505-1edc2de3c999">
-        <a>Good link</a>
+        <a>Good link test</a>
       </Link>
     </Layout>
   )
